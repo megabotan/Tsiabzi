e230f0ae7e854f3b3d1b48e33c3abc1ab62a46eb
dod <dod@dod-VirtualBox>
2013-01-24 08:01:42 +0200

    fail tests

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -10,7 +10,7 @@
 public class Sum {
     public int add(int a,int b)
     {
-        return a+b;
+        return a+b+1;
     }
     
     
--- src/test/java/SumTest.java
+++ src/test/java/SumTest.java
@@ -16,8 +16,13 @@ public class SumTest {
     }
 
     @org.junit.Test
-    public void testAdd() {
+    public void testAdd1() {
         Sum test=new Sum();
         assertEquals(test.add(1, 2),3);
+    }
+    @org.junit.Test
+    public void testAdd2() {
+        Sum test=new Sum();
+        assertEquals(test.add(-2, 2),0);
     }
 }
